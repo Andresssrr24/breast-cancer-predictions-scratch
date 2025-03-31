256425110a13195f4e08ac00675c01a042e31172
AndressR <andresrueda2406@gmail.com>
2025-03-31 16:04:03 -0500

    full nn

--- breast_cancer_pred_mlp.py
+++ breast_cancer_pred_mlp.py
@@ -2,7 +2,6 @@ import numpy as np
 from data_processing import process_data, dataset
 
 X_train, X_test, Y_train, Y_test = process_data(dataset)
-print(X_train.shape)
 
 class NeuralNet:
     def __init__(self, layers_dims):
@@ -24,9 +23,9 @@ class NeuralNet:
     
     # /---------------------------------------/
     # Forward propagation
-    def linear_forward(self, a, W, b):
-        Z = np.dot(W, a) + b
-        cache = (a, W, b)
+    def linear_forward(self, A, W, b):
+        Z = np.dot(W, A) + b
+        cache = (A, W, b)
 
         return Z, cache
 
@@ -37,8 +36,8 @@ class NeuralNet:
             A = self.relu(Z)
         elif activation == 'sigmoid':
             A = self.sigmoid(Z)
-        
-        activation_cache = z
+
+        activation_cache = Z
         cache = (linear_cache, activation_cache)
 
         return A, cache
@@ -52,32 +51,95 @@ class NeuralNet:
         for l in range(1, L):
             A_prev = A
 
-            AL, cache = self.forward_activation(A_prev, self.parameters[f'W{str(l)}'], 
-                                               self.parameters[f'b{str(L)}'], 'relu')
-
+            A, cache = self.forward_activation(A_prev, 
+                                               self.parameters[f'W{str(l)}'], 
+                                               self.parameters[f'b{str(l)}'], 
+                                               'relu')
             caches.append(cache)
 
-        AL, cache = self.forward_activation(A_prev, self.parameters[f'b{str(l)}'],
-                                            self.parameters[f'b{str(L)}'], 'sigmoid')
+        AL, cache = self.forward_activation(A, 
+                                            self.parameters[f'W{str(L)}'],
+                                            self.parameters[f'b{str(L)}'], 
+                                            'sigmoid')
         caches.append(cache)    
 
         return AL, caches
 
     # /---------------------------------------/
     # Loss computation  
-    def loss(self, AL, Y, parameters, lambd):
-        pass
+    def loss(self, AL, Y, lambd):
+        m = Y.shape[1]
+        AL = np.clip(AL, 1e-10, 1 - 1e-10)  # Avoid log 0
+
+        cost = -(1/m) * np.sum(Y*np.log(AL) + (1-Y) * np.log(1-AL))
+
+        L2 = 0
+        for key in self.parameters.keys():
+            if 'W' in key:
+                L2 += np.sum(np.square(self.parameters[key]))
+        L2 = (lambd/2) * L2
+
+        cost_L2 = cost + L2
+        cost_L2 = np.squeeze(cost_L2)
+
+        return cost_L2
+                             
     # /---------------------------------------/
     # Backpropagation
+    def linear_backward(self, dZ, cache):
+        A_prev, W, b = cache
+        m = A_prev.shape[1]
 
-    # /---------------------------------------/
-    # Activation functions
-    def sigmoid(self, Z):
-        return 1/(1+np.exp(-Z))
+        dW = 1/m * np.dot(dZ, A_prev.T)
+        db = 1/m * np.sum(dZ, axis=1, keepdims=True)
+        dA_prev = np.dot(W.T, dZ)
+
+        return dA_prev, dW, db
     
-    def relu(self, Z):
-        return np.maximum(0, Z)
+    def backward_activation(self, dA, cache, activation):
+        linear_cache, activation_cache = cache
+        Z = activation_cache
 
-forward_test = NeuralNet()
+        if activation == 'relu':
+            dZ = dA * (np.where(Z > 0, 1, 0))  # relu derivative
+        elif activation == 'sigmoid':
+            s = self.sigmoid(Z)
+            dZ = dA * (s * (1 - s))
 
+        dA_prev, dW, db = self.linear_backward(dZ, linear_cache)
 
+        return dA_prev, dW, db
+    
+    def backpropagation(self, AL, Y, caches):
+        grads = {}
+
+        L = len(caches)
+        m = AL.shape[1]
+        AL = np.clip(AL, 1e-10, 1 - 1e-10)  # Avoid Y/0
+        dAL = - (np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
+
+        current_cache = caches[L-1]
+        dA_prev_temp, dW_temp, db_temp = self.backward_activation(dAL, current_cache, 'sigmoid')
+        grads['dA' + str(L-1)] = dA_prev_temp
+        grads['dW' + str(L)] = dW_temp
+        grads['db' + str(L)] = db_temp
+
+        for l in reversed(range(L-1)):
+            current_cache = caches[l]
+            dA_prev_temp, dW_temp, db_temp = self.backward_activation(grads['dA' + str(l+1)], current_cache, 'relu')
+            grads['dA' + str(l)] = dA_prev_temp
+            grads['dW' + str(l+1)] = dW_temp
+            grads['db' + str(l+1)] = db_temp
+
+        return grads
+    
+    # /---------------------------------------/
+    # Optimization
+    def grad_desc(self, grads, lr):
+        L = len(self.parameters) // 2
+
+        for l in range(L):
+            self.parameters['W' + str(l+1)] = self.parameters['W' + str(l+1)] - lr * grads['dW' + str(l+1)]
+            self.parameters['b' + str(l+1)] = self.parameters['b' + str(l+1)] - lr * grads['db' + str(l+1)]
+
+        return self.parameters
--- data_processing.py
+++ data_processing.py
@@ -1,27 +1,28 @@
 import pandas as pd
-import numpy as np
 #from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
 dataset = pd.read_csv('data.csv')
-print(dataset.head())
+dataset = dataset.drop(columns='Unnamed: 32')
 
 def process_data(dataset, target_col='diagnosis', test_size=0.2, random_state=42):
     # Split data in X and Y
-    X = dataset.drop(columns=[target_col])
-    Y = dataset[target_col]
+    X = dataset.drop(columns=[target_col, 'id'])
+    Y = pd.get_dummies(dataset[target_col], drop_first=True).astype(int)
 
     # Shuffle data
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=test_size, random_state=random_state, shuffle=True)
 
     #Normalization
     scaler = StandardScaler()
-    X_train = scaler.fit_transform(X_train).T  # (32, 455)
-    X_test = scaler.fit_transform(X_test).T  # (32, 114)
+    X_train = scaler.fit_transform(X_train).T  # (30, 455)
+    X_test = scaler.fit_transform(X_test).T  # (30, 114)
 
     # Reshape Y
     Y_train = Y_train.values.reshape(1, -1)  # (1, 455)
     Y_test = Y_test.values.reshape(1, -1)  # (1, 114)
 
     return X_train, X_test, Y_train, Y_test
+
+X_train, X_test, Y_train, Y_test = process_data(dataset)
